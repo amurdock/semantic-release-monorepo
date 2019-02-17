2284dfc7375a866ec1a868448cd8cbe40b72daf2
Alasdair Murdock <alasdair.murdock@mudaless.com>
2019-02-17 15:41:24 +0000

    update decorator reference

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const logPluginVersion = require('./log-plugin-version');
 const {
   wrapPlugin,
   wrapMultiPlugin,
-} = require('semantic-release-plugin-decorators');
+} = require('@vehq/semantic-release-plugin-decorators');
 
 const {
   mapNextReleaseVersion,
